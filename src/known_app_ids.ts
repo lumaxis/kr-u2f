e50e57a4590ff47efaea422410190c696d49ef85
Lukas Spieß <lukas.spiess@gmail.com>
2020-04-03 11:29:10 +0200

    Upadte known_app_ids to current values

--- src/known_app_ids.ts
+++ src/known_app_ids.ts
@@ -1,17 +1,17 @@
 export function appIdToShortName(appId: string): string | undefined {
     const mapping = {
+        'github.com': 'gh',
         'https://api-9dcf9b83.duosecurity.com': 'dd',
         'https://bitbucket.org': 'b',
         'https://dashboard.stripe.com/u2f-facets': 's',
         'https://demo.yubico.com': 'yd',
-        'https://github.com/u2f/trusted_facets': 'gh',
         'https://gitlab.com': 'gl',
         'https://id.fedoraproject.org/u2f-origins.json': 'fd',
         'https://keepersecurity.com': 'kp',
-        'https://twitter.com/account/login_verification/u2f_trusted_facets.json': 'tw',
         'https://vault.bitwarden.com/app-id.json': 'vb',
         'https://www.dropbox.com/u2f-app-id.json': 'd',
         'https://www.gstatic.com/securitykey/origins.json': 'g',
+        'twitter.com': 'tw',
         'www.dropbox.com': 'd',
     };
 
